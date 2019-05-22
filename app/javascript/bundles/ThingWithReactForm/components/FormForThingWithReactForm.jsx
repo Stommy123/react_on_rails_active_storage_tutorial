3997287e8453cfa3295d7b83a4f6c8c255a22c43
Tommy <tsuwunrut@carecloud.com>
2019-05-22 13:25:36 -0400

    clean up

--- app/javascript/bundles/ThingWithReactForm/components/FormForThingWithReactForm.jsx
+++ app/javascript/bundles/ThingWithReactForm/components/FormForThingWithReactForm.jsx
@@ -1,65 +1,44 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const token   = document.querySelector('meta[name="csrf-token"]')
-                .getAttribute('content')
+const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 
 const headers = {
-                  'X-Requested-With': 'XMLHttpRequest',
-                  'X-CSRF-TOKEN':     token,
-                  'content-type':     'multipart/form-data'
-                }
+  'X-Requested-With': 'XMLHttpRequest',
+  'X-CSRF-TOKEN': token,
+  'content-type': 'multipart/form-data'
+};
 
 class FormForThingWithReactForm extends Component {
+  state = { name: '', image: '' };
 
-  state = { name: '', image: '' }
+  handleNameChange = e => this.setState({ name: e.target.value });
 
-  handleNameChange = event => {
-    const name = event.target.value
-    this.setState({ name })
-  }
+  handleImageChange = e => this.setState({ image: e.target.files[0] });
 
-  handleImageChange = event => {
-    const image = event.target.files[0]
-    this.setState({ image })
-  }
+  handleSubmit = async event => {
+    event.preventDefault();
+    const formData = new FormData();
+    Object.keys(this.state).forEach(state => formData.append(`thing_with_react_form[${state}]`, this.state[state]));
+    const { data } = await axios.post('/thing_with_react_forms.json', formData, { headers });
+    Turbolinks.visit(`/thing_with_react_forms/${data.id}`);
+  };
 
-  handleSubmit = event => {
-    event.preventDefault()
-    const formData = new FormData()
-    formData.append('thing_with_react_form[name]', this.state.name)
-    formData.append('thing_with_react_form[image]', this.state.image)
-
-    axios.post('/thing_with_react_forms.json', formData, { headers: headers }
-      ).then((response) => {
-        Turbolinks.visit(`/thing_with_react_forms/${response.data.id}`)
-      })
-  }
-
-  render(){
-    return(
+  render() {
+    return (
       <form onSubmit={this.handleSubmit}>
-        <div className='field'>
-          <label htmlFor='name'>Name</label>
-          <input
-            name='name'
-            type='text'
-            value={this.state.name}
-            onChange={this.handleNameChange}
-          />
+        <div className="field">
+          <label htmlFor="name">Name</label>
+          <input name="name" type="text" value={this.state.name} onChange={this.handleNameChange} />
         </div>
-        <div className='field'>
-          <label htmlFor='image'>Image</label>
-          <input
-            name='image'
-            type='file'
-            onChange={this.handleImageChange}
-          />
+        <div className="field">
+          <label htmlFor="image">Image</label>
+          <input name="image" type="file" onChange={this.handleImageChange} />
         </div>
         <button type="submit">Please Work</button>
       </form>
-    )
+    );
   }
 }
 
-export default FormForThingWithReactForm
+export default FormForThingWithReactForm;
